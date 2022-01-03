a7155505cbebddd65a0f5b64d896dcd778a8d8cb
jenhuynh <jendoubleh@gmail.com>
2022-01-03 13:21:44 -0800

    Add StaticTest/Super problem (practice using static methods)

---
+++ StaticSuper.java
@@ -0,0 +1,13 @@
+class StaticSuper {
+    static {
+        System.out.println("super static block");
+    }
+
+    StaticSuper() {
+        System.out.println(
+                "super constructor");
+    }
+}
+
+
+
