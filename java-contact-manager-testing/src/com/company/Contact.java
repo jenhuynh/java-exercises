44395df635a0a848b9e86783f07c096eb6e10887
jenhuynh <jendoubleh@gmail.com>
2022-01-10 10:44:08 -0800

    Add contact application for testing

---
+++ java-contact-manager-testing/src/com/company/Contact.java
@@ -0,0 +1,8 @@
+package com.company;
+//contact manager application, it manages the contact information
+    //contains 3 fields firstName, lastName, and phoneNumber
+public class Contact {
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ java-contact-manager-testing/src/com/company/ContactManager.java
@@ -0,0 +1,8 @@
+package com.company;
+//ContactManager is responsible to store the contact information
+public class ContactManager {
+
+    //addContact method takes the first and last name as input parameters and then storing information
+        // in memory using a concurrent hash map
+        //using concurrent hashmap because it's a thread safe 
+}
